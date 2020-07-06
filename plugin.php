84707641c7352373365859ee811e8afa80d685b7
Jonas Schelde <kontakt@jonasschelde.dk>
2020-07-06 22:57:48 +0200

    v2.5.0
    Added: Plugin updater
    Added: Initization lines for plugin updater to plugin file.
    Added: vendor folder

--- plugin.php
+++ plugin.php
@@ -18,6 +18,22 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+
+// Load - all classes as services in global scope, load hooks from constructors of classes
 require __DIR__ . '/vendor/autoload.php';
 
 $plugin_name_settings = new plugin_name\plugin_name_settings(__FILE__);
+
+
+// Plugin updater
+require 'plugin-update-checker/plugin-update-checker.php';
+
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/username/wordpress-plugin-plugin_name/',
+    __FILE__,
+    'ritzau'
+);
+
+$myUpdateChecker->setAuthentication('wordpresspluginplugin_nameaccess_token');
+
+$myUpdateChecker->setBranch('master');
